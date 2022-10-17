19233fa1b9ad46f3ed87540831f4cb28b3e806da
Paul Hultgren <39424187+pjhul@users.noreply.github.com>
2022-10-17 10:28:25 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/* global module */
+/* eslint no-undef: "error" */
+
+// Some internal library function
+async function getRandomNumber() {
+    return 4
+}
+
+// Plugin method that runs on plugin load
+async function setupPlugin({ config }) {
+    console.log(config.greeting)
+}
+
+// Plugin method that processes event
+async function processEvent(event, { config, cache }) {
+    const counterValue = (await cache.get('greeting_counter', 0))
+    cache.set('greeting_counter', counterValue + 1)
+    if (!event.properties) event.properties = {}
+    event.properties['greeting'] = config.greeting
+    event.properties['greeting_counter'] = counterValue
+    event.properties['random_number'] = await getRandomNumber()
+    return event
+}
+
+// The plugin itself
+module.exports = {
+    setupPlugin,
+    processEvent
+}
